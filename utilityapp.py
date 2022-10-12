d7685d81eceb280d22888b01ddefae7f69dd9510
manisha22-soli <115647465+manisha22-soli@users.noreply.github.com>
2022-10-12 11:15:46 -0400

    Add files via upload

---
+++ utilityapp.py
@@ -0,0 +1,73 @@
+#this is the script where I will try to build this utility
+import fileinput
+import streamlit as st
+from outputcleaner import clean_output
+import pandas as pd
+import webvtt
+from io import StringIO
+
+#base = "light"
+#primaryColor="#001BFF"
+#backgroundColor="#FFFFFF"
+#secondaryBackgroundColor="#F0F2F6"
+#textColor="#000B62"
+#font="monospace"
+
+st.set_page_config('Transcript Cleaner')
+header = st.container()
+file_upload = st.container()
+dataoutput = st.container()
+
+with header:
+    st.title('Web Transcript File Cleaner')
+
+with file_upload:
+    st.subheader('Upload Web Transcript File Here')
+    file = st.file_uploader("Choose a file", type='vtt')
+    transcript = ""
+    finalitem = ' '
+    finallist = []
+    if file is not None:
+        file_details = {"FileName": file.name, "FileType": file.type,
+        "FileSize": file.size}
+        st.write(file_details)
+        vtt = file.getvalue()
+        vtt = vtt.decode("utf-8")
+        vttlist = []
+        newvtt = []
+        vttlist = vtt.split('\n')
+        for i in vttlist:
+            if i:
+                newvtt.append(i)
+        alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z','A','B',
+        'C','D','E','F','G','H', 'I', 'J','K','L','M','N','O','P','Q','R',
+        'S','T','U','V','W','X','Y','Z']
+        for item in newvtt:
+            if item[0] in alphabet:
+                finallist.append(item)
+
+        finalitem = '\n'.join(finallist)
+
+        #vtt = webvtt.read(file)
+
+        #check and see if webvtt works if user inputs file path instead of file directly
+        #rd = webvtt.read_buffer(uploaded_file)
+        #vtt = uploaded_file.getvalue
+        #st.write(vtt)
+
+        #removes blank lines and lines that start with numbers
+
+
+
+        #previous = None
+        #for line in lines:
+            #if line == previous:
+                #continue
+            #transcript += line + "\n"
+            #previous = line
+
+
+with dataoutput:
+    st.subheader('Download Clean Transcript Here')
+    st.download_button('Download Cleaned Output', finalitem, 'clean.txt', 'Text')

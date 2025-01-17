99e342f7b3ca290aa636794df0a23e59c40a75b0
manisha-solipuram <115647465+manisha-solipuram@users.noreply.github.com>
2025-01-17 13:02:47 -0500

    cleanup

--- utilityapp.py
+++ utilityapp.py
@@ -16,7 +16,7 @@ st.markdown(""" <style>
 
 
 with header:
-    st.title('WebVTT Transcript File Cleaner')
+    st.title('Zoom Transcript File Cleaner')
 
 with file_upload:
     st.subheader('Upload Web Transcript File Here')

1f5ef3734080c9afe8426e2e32649d1f6e1e71f7
manisha-solipuram <115647465+manisha-solipuram@users.noreply.github.com>
2025-01-17 12:47:26 -0500

    cleanup

--- utilityapp.py
+++ utilityapp.py
@@ -13,7 +13,7 @@ dataoutput = st.container()
 st.markdown(""" <style>
    footer {visibility: hidden;}
    </style> """, unsafe_allow_html=True)
-  
+
 
 with header:
     st.title('WebVTT Transcript File Cleaner')
@@ -21,37 +21,37 @@ with header:
 with file_upload:
     st.subheader('Upload Web Transcript File Here')
     file = st.file_uploader("Choose a file", type='vtt')
-    finalitem = ' '
-    finallist = []
     if file is not None:
         file_details = {"FileName": file.name, "FileType": file.type,
         "FileSize": file.size}
         st.write(file_details)
-        vtt = file.getvalue()
-        vtt = vtt.decode("utf-8")
-        # vttlist = []
-        newvtt = []
-        vttlist = vtt.split('\n')
+
+        vttlist = file.getvalue().decode("utf-8").split('\n')
         newvtt = [i for i in vttlist if i]
-        # for i in vttlist:
-        #     if i:
-        #         newvtt.append(i)
 
         currentname = ' '
-         
+        currentstring = []
+        finallist = []
         for item in newvtt[1:]:
-            if '"' in item:
-               sp = item.split('"')
-               name = sp[1]
-               if name != currentname:
-                  finallist.append(name)
-                  currentname = name
-            elif item[0] in list(string.ascii_letters):
-                finallist.append(item)
+            if item[0] in list(string.ascii_letters):
+                item_list = item.split(': ')
+                name = item_list[0]
+                if name != currentname:
+                    finallist.append(f"{' '.join(currentstring)}\n")
+                    currentname = name
+                    currentstring = [item]
+                else:
+                    currentstring.append(f"{item_list[1]}")
 
-        finalitem = '\n'.join(finallist)
+        finallist.append(f"{' '.join(currentstring)}\n")
+        finalitem = '\n'.join(finallist[1:])
 
 
 with dataoutput:
     st.subheader('Download Clean Transcript Here')
-    st.download_button('Download Cleaned Output', finalitem, 'clean.txt', 'Text')
+
+    try:
+        if finalitem:
+            st.download_button('Download Cleaned File', finalitem, f"{file.name[:-4]}_cleaned.txt", 'Text')
+    except:
+        st.write("")
